8096f906cafe6c3e89cff8ccd7b0c1f1c14ab459
erlanoo <102740851+erlanoo@users.noreply.github.com>
2022-06-16 18:08:56 +0300

    Delete keyboards directory

--- keyboards/client_kb.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-start_button = KeyboardButton("/start")
-quiz_button = KeyboardButton("/quiz")
-
-start_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-
-start_markup.row((start_button,quiz_button))
